c759d0bd2445dd99a71c787eb850e8eb320c1b2b
ICE <ice@lab06m27.nrc.ice.ufjf.br>
2019-09-13 19:43:47 -0300

    Kirby

---
+++ Kirby/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float spped;
+
+    private Animator animator;
+    private SpriteRenderer renderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx = Input.GetAxis("Horizontal");
+        transform.Translate(dx * spped * Time.deltaTime, 0, 0);
+
+        if (Mathf.Abs(dx) > 0.000001f)
+            animator.SetBool("walking", true);
+        else
+            animator.SetBool("walking", false);
+
+
+        if (dx < 0)
+            renderer.flipX = true;
+        else
+            renderer.flipX = false;
+    }
+}
